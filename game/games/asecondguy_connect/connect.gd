071c228a3e9894a88c0ec0ef7830c35c945e542f
ASecondGuy <asecondguy@gmail.com>
2022-06-13 09:38:53 +0200

    connect 4 ending working

--- game/games/asecondguy_connect/connect.gd
+++ game/games/asecondguy_connect/connect.gd
@@ -1,7 +1,16 @@
 extends Node2D
 
+onready var _play_area := $PlayArea
+
 var _last_chip: RigidBody2D
 
+func _ready():
+	_empty()
 
 func _empty():
 	$bounds/bottom.disabled = true
+
+
+func _on_PlayArea_body_exited(_body):
+	if _play_area.get_overlapping_bodies().size() == 0:
+		GameManager.end_game()
